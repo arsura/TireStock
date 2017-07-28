f793bc2aa632d069f3a30e8b7ad6d5c6374fe486
arsura <arsurarizen@gmail.com>
2017-07-28 21:34:23 +0700

    initial commit

--- index.php
+++ index.php
@@ -14,28 +14,27 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="http://getbootstrap.com/favicon.ico">
 
-    <title>Cover Template for Bootstrap</title>
+    <title>โปรแกรมจัดการรายการสินค้า</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="http://getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="index_style/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="index_style/cover.css" rel="stylesheet">
-	<link href="index_style/hover.css" rel="stylesheet">
+	  <link href="index_style/hover.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="http://getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
+    <script src="index_style/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <script src="index_style/html5shiv.min.js"></script>
+      <script src="index_style/respond.min.js"></script>
     <![endif]-->
   </head>
 
@@ -60,12 +59,11 @@
 
           <div class="inner cover">
             <h1 class="cover-heading">จัดการสินค้า</h1>
-            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-            <p class="lead">
-              <a href="manage.php" class="btn btn-lg btn-default">เพิ่มรายการสินค้า</a>
-			  <a href="add_item.php" class="btn btn-lg btn-default">เพิ่มจำนวนสินค้า</a>
-			  <a href="sell_item.php" class="btn btn-lg btn-default">ขายสินค้า</a>
-			  <a href="report.php" class="btn btn-lg btn-default">รายงานผล</a>
+              <a href="manage.php" class="btn btn-lg btn-default">เพิ่มรายการสินค้า</a> 
+              <a href="add_item.php" class="btn btn-lg btn-default">เพิ่มจำนวนสินค้า</a> 
+              <a href="sell_item.php" class="btn btn-lg btn-default">ขายสินค้า</a> 
+              <a href="cancel_order.php" class="btn btn-lg btn-default">ยกเลิกออร์เดอร์</a> 
+              <a href="report.php" class="btn btn-lg btn-default">รายงานผล</a> 
             </p>
           </div>
 
@@ -87,11 +85,11 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js\bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="index_style/ie10-viewport-bug-workaround.js"></script>
   </body>
 
 
